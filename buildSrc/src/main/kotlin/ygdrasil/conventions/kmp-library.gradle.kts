b5181e88d0a54ec3fa5f01e88dec1880c13e8b18
ygdrasil-io <alexandre.mommers@gmail.com>
2026-06-14 01:33:45 +0200

    GRA-77: add UIUtilities.tbd stub + linker search path

--- buildSrc/src/main/kotlin/ygdrasil/conventions/kmp-library.gradle.kts
+++ buildSrc/src/main/kotlin/ygdrasil/conventions/kmp-library.gradle.kts
@@ -2,11 +2,14 @@
 package ygdrasil.conventions
 
 import com.android.build.api.variant.KotlinMultiplatformAndroidComponentsExtension
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 plugins {
     id("org.jetbrains.kotlin.multiplatform")
     id("com.android.kotlin.multiplatform.library")
 }
+
+val konanStubs = project.rootProject.projectDir.resolve("konan-stubs")
 
 kotlin {
     jvmToolchain(25)
@@ -17,6 +20,12 @@ kotlin {
 
     iosArm64()
     iosSimulatorArm64()
+
+    targets.withType<KotlinNativeTarget>().configureEach {
+        binaries.all {
+            linkerOpts("-F$konanStubs")
+        }
+    }
 }
 
 extensions.configure<KotlinMultiplatformAndroidComponentsExtension> {
