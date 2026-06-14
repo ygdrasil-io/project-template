f71407284963865c508df748b5fcedf5d7fb4db7
ygdrasil-io <alexandre.mommers@gmail.com>
2026-06-14 22:17:46 +0200

    fix: use Central Portal staging API (ossrh-staging-api.central.sonatype.com)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
 nexusPublishing {
     repositories {
         create("mavenCentral") {
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
             username.set(project.findProperty("mavenCentralUsername") as? String ?: System.getenv("MAVEN_CENTRAL_USERNAME") ?: "")
             password.set(project.findProperty("mavenCentralPassword") as? String ?: System.getenv("MAVEN_CENTRAL_PASSWORD") ?: "")
         }
