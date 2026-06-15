f88623bf755b59d302032bcd73c90955a6afa62c
ygdrasil-io <alexandre.mommers@gmail.com>
2026-06-15 16:14:18 +0200

    ci: add KotlinNativeCacheApi opt-in via gradle.properties

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,14 +10,6 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-    buildscript {
-        dependencies {
-            classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.0")
-        }
-    }
-    plugins {
-        id("org.jetbrains.kotlin.multiplatform") version "2.4.0" apply false
-    }
 }
 
 dependencyResolutionManagement {
@@ -25,12 +17,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-    }
-}
-
-configure<org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xopt-in=org.jetbrains.kotlin.gradle.plugins.KotlinNativeCacheApi")
     }
 }
 
